a8d96e23939228699357dc7ab0ea6e25153ab739
tilenur <tilenur@gmail.com>
2026-02-24 20:57:08 +0600

    update

--- oop.py
+++ oop.py
@@ -1,77 +1,91 @@
-#1 Инкапсуляция
+# 1) Инкапсуляция
 
-# class Person:
-#     def __init__(self):
-#         self._age = 0
-#
-#     def set_age(self, age):
-#         if age < 0:
-#             raise ValueError("Age cannot be negative")
-#         self._age = age
-#
-#     def get_age(self):
-#         return self._age
-#
-# p = Person()
-# p.set_age(25)
-# print(p.get_age())   # 25
-# p.set_age(-5)
+class Person:
+    def __init__(self):
+        self._age = 0
 
-#2 Наследование
+    def set_age(self, age):
+        if age < 0:
+            raise ValueError("Age cannot be negative")
+        self._age = age
 
-# class Animal:
-#     def __init__(self, name):
-#         self.name = name
-#
-#     def speak(self):
-#         return "I am an animal"
-#
-# class Dog(Animal):
-#     def speak(self):
-#         return "Woof"
-#
-# class Cat(Animal):
-#     def speak(self):
-#         return "Meow"
-#
-# dog = Dog("Buddy")
-# cat = Cat("Kitty")
-#
-# print(dog.name, dog.speak())  # Вывод: Buddy Woof
-# print(cat.name, cat.speak())  # Вывод: Kitty Meow
+    def get_age(self):
+        return self._age
 
-# 3. Полиморфизм
 
-# class Vehicle:
-#     def move(self):
-#         return "Vehicle is moving"
-#
-# class Car:
-#     def move(Vehicle):
-#         return "Car is driving"
-#
-# class Bicycle:
-#     def move(Vehicle):
-#         return "Bicycle is pedaling"
-#
-# def move(vehicle):
-#     return vehicle.move()
-#
-# car = Car()
-# bike = Bicycle()
-#
-# print(move(car))  # Вывод: Car is driving
-# print(move(bike))  # Вывод: Bicycle is pedaling
+p = Person()
+p.set_age(25)
+print(p.get_age())   # 25
+# p.set_age(-5)      # ValueError
 
-#  4. Абстракция
+
+# 2) Наследование
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        return "I am an animal"
+
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof"
+
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow"
+
+
+dog = Dog("Buddy")
+cat = Cat("Kitty")
+
+print(dog.name, dog.speak())  # Buddy Woof
+print(cat.name, cat.speak())  # Kitty Meow
+
+
+# 3) Полиморфизм
+
+class Vehicle:
+    def move(self):
+        return "Vehicle is moving"
+
+
+class Car(Vehicle):
+    def move(self):
+        return "Car is driving"
+
+
+class Bicycle(Vehicle):
+    def move(self):
+        return "Bicycle is pedaling"
+
+
+def move(vehicle):
+    return vehicle.move()
+
+
+car = Car()
+bike = Bicycle()
+
+print(move(car))   # Car is driving
+print(move(bike))  # Bicycle is pedaling
+
+
+# 4) Абстракция
 
 from abc import ABC, abstractmethod
+import math
+
 
 class Shape(ABC):
 
     @abstractmethod
     def area(self):
         pass
+
 
 class Rectangle(Shape):
     def __init__(self, width, height):
@@ -87,7 +101,7 @@ class Circle(Shape):
         self.radius = radius
 
     def area(self):
-        return 3.14 * self.radius ** 2
+        return math.pi * self.radius ** 2
 
 
 rect = Rectangle(10, 5)
